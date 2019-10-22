3a8c66f3cfa83a421eb89166f9481d7479b16ee6
daukadolt <daulet.amirkhanov.official@gmail.com>
2019-10-22 12:38:51 +0600

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VIndex/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VIndex from "./components/VIndex.vue"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    VIndex
   }
 }
 </script>
