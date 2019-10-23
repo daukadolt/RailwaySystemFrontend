c2c33625ac48a4888690a6ef21d1cdc1ade5e54d
daukadolt <daulet.amirkhanov.official@gmail.com>
2019-10-23 08:24:50 +0600

    preliminary component division

--- src/components/VIndex.vue
+++ src/components/VIndex.vue
@@ -1,9 +1,24 @@
 <template>
-    
+    <div>
+        <div class="bg">
+            <VNavbar/>
+            <VFromToSearch/>
+        </div>
+    </div>
 </template>
 
 <script>
+import VNavbar from "./VNavbar.vue";
+import VFromToSearch from "./VFromToSearch.vue";
+
 export default {
-    name: "VIndex"
+    name: "VIndex",
+    components: {
+        VNavbar,
+        VFromToSearch
+    }
 }
 </script>
+
+<style scoped>
+</style>
