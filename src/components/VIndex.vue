976112fefd99b79b8dd6024cffdfcd9bf560d31c
daukadolt <daulet.amirkhanov.official@gmail.com>
2019-10-24 00:43:12 +0600

    latest version to distribute tasks to team

--- src/components/VIndex.vue
+++ src/components/VIndex.vue
@@ -3,6 +3,7 @@
         <div class="bg">
             <VNavbar/>
             <VFromToSearch/>
+            <VMainPageTableAndMap/>
         </div>
     </div>
 </template>
@@ -10,12 +11,25 @@
 <script>
 import VNavbar from "./VNavbar.vue";
 import VFromToSearch from "./VFromToSearch.vue";
+import VMainPageTableAndMap from "./VMainPageTableAndMap.vue";
 
 export default {
     name: "VIndex",
+    data: function() {
+        return {
+            currentTableComponent: 'VFromToSearch'
+        }
+    },
     components: {
         VNavbar,
-        VFromToSearch
+        VFromToSearch,
+        VMainPageTableAndMap
+    },
+    mounted: function(){},
+    computed: {
+        currentTableComputed: function() {
+            return this.currentTableComponent
+        }
     }
 }
 </script>
---
+++ src/components/VMainPageTableAndMap.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        # write table here
+    </div>
+</template>
