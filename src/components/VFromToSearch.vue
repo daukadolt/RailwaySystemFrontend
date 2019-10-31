d9ecc8c636f8d376ce487e596ecccdad597993f4
Lunara07 <luna.1998.ln@gmail.com>
2019-10-31 22:13:05 +0600

    Style has been added to Navbar, Table, FromToSearch

--- src/components/VFromToSearch.vue
+++ src/components/VFromToSearch.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
+   
+    <div class="form-inline"> 
+        <div class="text">Search the routes</div>
         <form @submit.prevent="searchFromTo">
-            <input type="text" placeholder="from city" v-model="fromCity">
-            <input type="text" placeholder="to city" v-model="toCity">
-            <button type="submit">Search</button>
+            <input type="text" placeholder="From City" v-model="fromCity" class="input-box">
+            <input type="text" placeholder="To City" v-model="toCity" class="input-box">
+            <button type="submit" class="btn" >Search</button>
         </form>
         {{ info }}
     </div>
@@ -30,3 +32,37 @@ export default {
     }
 }
 </script>
+<style scoped>
+    .text{
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        font-size: 50px;
+    }
+    .form-inline{
+        display: inline-block;
+    }
+    .input-box {
+        width: 150px;
+        height: 34px;
+        
+        padding: 6px 3px;
+        margin-right: 10px;
+        font-size: 14px;
+        color: #555;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+    .btn {
+        display: inline-block;
+        padding: 6px 12px;
+        margin-bottom: 0;
+        font-size: 14px;
+        font-weight: 400;
+        text-align: center;
+        vertical-align: middle;
+        -ms-touch-action: manipulation;
+        touch-action: manipulation;
+        cursor: pointer;
+        border: 1px solid transparent;
+        border-radius: 4px;
+    }
+</style>
--- src/components/VMainPageTableAndMap.vue
+++ src/components/VMainPageTableAndMap.vue
@@ -1,5 +1,60 @@
 <template>
-    <div>
-        # write table here
-    </div>
+<div class="tableFrom">
+    <table>
+        <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+        <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+        </tr>
+        <tr>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+        </tr>
+        <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+        </tr>
+        <tr>
+            <td>Laughing Bacchus Winecellars</td>
+            <td>Yoshi Tannamuri</td>
+            <td>Canada</td>
+        </tr>
+        <tr>
+            <td>Magazzini Alimentari Riuniti</td>
+            <td>Giovanni Rovelli</td>
+            <td>Italy</td>
+        </tr>
+        </table>
+</div>
 </template>
+<style scoped>
+    table {
+        font-family: arial, sans-serif;
+        display: inline;        
+    }
+
+    td, th {
+        margin-top:40px;
+        border: 1px solid #dddddd;
+        padding: 8px;
+    }
+
+    tr:nth-child(odd) {
+        background-color:#fff ;
+    }
+    tr:nth-child(even) {
+        background-color:rgb(158, 155, 155) ;
+    }
+</style>
