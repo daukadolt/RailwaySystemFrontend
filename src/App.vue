583a45d6764ceddc1e933aec86639262b98ae083
danikk2398 <danik_2398@mail.ru>
2019-11-22 14:43:38 +0600

    Loading component was created and added to Appvue

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,26 @@
   <div id="app">
     <VNavbar/>
     <router-view/>
+    <div v-if="showLoadingSpinner" class="loadingSpinner" id="popup"><LoadingSpinner/></div>
   </div>
   </div>
 </template>
 
 <script>
 import VNavbar from "./components/VNavbar.vue";
+import LoadingSpinner from "./components/Loading.vue";
 
 export default {
   name: 'app',
   data() {
       return {
-          image : 'https://blog.eurail.com/wp-content/uploads/2016/12/Train-in-Swiss-Alps.jpg'
+          image : 'https://blog.eurail.com/wp-content/uploads/2016/12/Train-in-Swiss-Alps.jpg',
+          showLoadingSpinner: true
       }
     },
   components: {
-    VNavbar
+    VNavbar,
+    LoadingSpinner
   }
 }
 </script>
@@ -43,5 +47,38 @@ body {
   background-position: absolute;
   margin:0;
   overflow-y: auto;
+}
+.loadingSpinner{
+  position: fixed;
+  padding: 10px;
+  width: 280px;
+  left: 50%;
+  margin-left: -150px;
+  height: 180px;
+  top: 50%;
+  margin-top: -100px;
+  background: transparent;;
+  z-index: 20;
+}
+#popup:after {
+  position: fixed;
+  content: "";
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0,0,0,0.9);
+  z-index: -2;
+}
+
+#popup:before {
+  position: absolute;
+  content: "";
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background:transparent;
+  z-index: -1;
 }
 </style>
